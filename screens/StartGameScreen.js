fda361077828885a32e88a176b4e8d923ae1d1cb
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-25 04:28:19 +0530

    50 style: Added css to TextInput and maxLength attribute

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,8 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      {/* 50 Added style and maxLength*/}
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -14,6 +15,7 @@ function StartGameScreen() {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
+  // 49
   inputContainer: {
     marginTop: 100,
     marginHorizontal: 24,
@@ -25,5 +27,17 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  // 50
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center', // Align text to center of text box / TextInput
   },
 });
