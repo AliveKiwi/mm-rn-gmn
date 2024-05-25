db0f54ceb8cb81d5d49e67786746333f4f3ca4e7
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-25 22:20:32 +0530

    60 style: Setting basic structure GameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,12 +2,23 @@ import { StyleSheet, Text, View } from 'react-native';
 
 function GameScreen() {
   return (
-    <View>
-      <Text>GameScreen</Text>
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
     </View>
   );
 }
 
 export default GameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
