7d0d5e0a3081d1ec2fb9b5062a248e63073438f0
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-25 21:49:33 +0530

    58 func: Added logic to confirmInputHandler and an alert popup on invalid inputs

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { TextInput, View, StyleSheet } from 'react-native';
+import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen() {
@@ -11,8 +11,25 @@ function StartGameScreen() {
     setEnteredNumber(enteredText);
   }
 
-  // 57
-  function confirmInputHandler() {}
+  // 58  reset number to empty string / initial state
+  function resetInputHandler() {
+    setEnteredNumber('');
+  }
+
+  // 58
+  function confirmInputHandler() {
+    const choosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(choosenNumber) || choosenNumber <= 0 || choosenNumber > 99) {
+      // SHOW ALERT ... alert(title,message,{behaviour})
+      Alert.alert(
+        'Invalid number!',
+        'Number has to be a number between 1 and 99',
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+      );
+      return;
+    }
+  }
 
   return (
     <View style={styles.inputContainer}>
@@ -33,7 +50,8 @@ function StartGameScreen() {
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          {/* 58 added onPress*/}
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
           {/* 57 added onPress*/}
