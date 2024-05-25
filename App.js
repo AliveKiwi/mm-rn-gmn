ba355ba1d519e0110d0c0b2f2b8ed90c45e5eb37
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-25 21:12:04 +0530

    56 style: Added background image using ImageBackground componenet

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
@@ -7,7 +7,15 @@ export default function App() {
   return (
     // 55 Use npx expo install install expo-linear-gradient to ensure compatibility of package with SDK
     <LinearGradient colors={['#ddb52f', '#4e0329']} style={styles.rootScreen}>
-      <StartGameScreen />
+      {/* 56 */}
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -16,4 +24,5 @@ const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
   },
+  backgroundImage: { opacity: 0.15 },
 });
