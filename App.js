e18b75c3112cb63d15e9b70a07f69e565614f679
AliveKiwi <prateekjais.dev@gmail.com>
2024-05-26 17:09:31 +0530

    61 component: Wraped screen in SafeAreaView component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
@@ -34,7 +34,9 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        {/* 61 Added SafeAreaView to get content under notch, works for iOS
+         */}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
@@ -43,6 +45,7 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+    paddingTop: 10,
   },
   backgroundImage: { opacity: 0.15 },
 });
